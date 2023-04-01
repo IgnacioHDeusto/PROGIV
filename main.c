b7e969bebb4afb5c335ff61ca59ae1683d845d32
etxan <etxan@192.168.1.145>
2023-04-01 15:01:46 +0200

    2124124141234123

--- main.c
+++ main.c
@@ -10,5 +10,13 @@ int main() {
 	printf("Borja gay");
 	printf("Borja gay");
 
+	printf("Borja gay");
+		printf("Borja gay");
+		printf("Borja gay");
+		printf("Borja gay");
+		printf("Borja gay");
+		printf("Borja gay");
+		printf("Borja gay");
+
 	return 1;
 }
